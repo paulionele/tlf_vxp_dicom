e8eca0a8600f5062c1b317e226cc1fc2eb5b0155
paulionele <p.ionele@gmail.com>
2017-07-31 14:54:47 -0700

    Minor changes.

--- waveform_generator.m
+++ waveform_generator.m
@@ -1,11 +1,21 @@
 function [rpm_times, phase] = waveform_generator(period)
-%waveform_generator is a function that replaces the mw_reader and
-%vxp_reader functions, for testing purposes.
+%This function can be called instead of the mw_reader and vxp_reader
+%functions, for testing purposes. The user specifies a
+%period in seconds, and the function returns an array of phase values
+%between 0 and 2*pi as a function of rpm_times.
+
+%INPUT: user specified period in seconds.
+%OUTPUT: rpm_times with pre-defined time interval and duration. phase array
+%with discrete values between 0 and 2*pi, with a 1:1 mapping to rpm_times.
+
 %   A baseline performance assesment of the 4D VMAT calculation can be
 %   performed using a preprogrammed repiratory waveform. No phase shift
 %   applied; phase begins at zero phase.
 
 period_predef = 4; %4 second respiratory period
+sampling_interval = 30; %in ms
+sampling_duration = 200000; %1000 ms = 1 s
+rpm_times = 0:sampling_interval:sampling_duration;
 
 if length(period) == 0
    period = period_predef;
@@ -16,11 +26,6 @@ else
    disp(message)
 end
 
-sampling_interval = 30; %in ms
-sampling_duration = 200000; %1000 ms = 1 s
-rpm_times = 0:sampling_interval:sampling_duration;
-
 theta = 2*pi*rpm_times/(period*1000); %working in ms
 phase = mod(theta - 2*pi, 2*pi); %returned phases on [0,2*pi)
 end
-
